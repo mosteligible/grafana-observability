62c456d3497a39c2ca151f514722a1ef9b0a4a5d
mosteligible <amtrijal147@gmail.com>
2025-06-13 00:00:46 -0400

    sys metrics for collector

--- app/app.py
+++ app/app.py
@@ -6,14 +6,17 @@ from pydantic import BaseModel
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 # from opentelemetry.exporter.prometheus import PrometheusMetricReader # <--- REMOVE THIS
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.metrics import MeterProvider
 # Use PeriodicExportingMetricReader for OTLP push
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.instrumentation.system_metrics import SystemMetricsInstrumentor
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader, ConsoleMetricExporter
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+# from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 
 from opentelemetry import trace, metrics
 
@@ -32,16 +35,37 @@ trace.set_tracer_provider(
 )
 otlp_span_exporter = OTLPSpanExporter(
     endpoint="http://10.0.0.217:4317"
+    # endpoint="http://10.0.0.217:5080/api/default/v1/traces",
+    # headers=OPENOBSERVE_HEADERS,
 )
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_span_exporter))
 
 # METRICS TRACKING
-otlp_metric_exporter = OTLPMetricExporter(endpoint="http://10.0.0.217:4317", insecure=True)
+otlp_metric_exporter = OTLPMetricExporter(
+    endpoint="http://10.0.0.217:4317"
+    # endpoint="http://10.0.0.217:5080/api/default/v1/metrics",
+    # headers=OPENOBSERVE_HEADERS
+)
 metric_reader = PeriodicExportingMetricReader(otlp_metric_exporter, export_interval_millis=5000)
+metrics.set_meter_provider(
+    meter_provider=MeterProvider(
+        [
+            metric_reader,
+            PeriodicExportingMetricReader(ConsoleMetricExporter(), export_interval_millis=10000)
+        ]
+    )
+)
 # Create a MeterProvider
-meter_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
-# Set the global MeterProvider
-metrics.set_meter_provider(meter_provider)
+configuration = {
+    "system.memory.usage": ["used", "free", "cached"],
+    "system.cpu.time": ["idle", "user", "system", "irq"],
+    "system.network.io": ["transmit", "receive"],
+    "process.memory.usage": None,
+    "process.memory.virtual": None,
+    "process.cpu.time": ["user", "system"],
+    "process.context_switches": ["involuntary", "voluntary"],
+}
+SystemMetricsInstrumentor(config=configuration).instrument()
 
 #### LOG EXPORTS
 logger = logging.getLogger(name="this-logger")
@@ -61,19 +85,22 @@ from opentelemetry.instrumentation.logging import LoggingInstrumentor
 #         self._logger.emit(self._translate(record))
 
 
-logger_provider = LoggerProvider(resource=resource)
-set_logger_provider(logger_provider)
-log_exporter = OTLPLogExporter(endpoint="http://10.0.0.217:4318")
-logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
+# logger_provider = LoggerProvider(resource=resource)
+# set_logger_provider(logger_provider)
+# log_exporter = OTLPLogExporter(
+#     endpoint="http://10.0.0.217:5080/api/default/v1/logs",
+#     headers=OPENOBSERVE_HEADERS,
+# )
+# logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
 
-otel_log_handler = LoggingHandler(logger_provider=logger_provider)
+# otel_log_handler = LoggingHandler(logger_provider=logger_provider)
 LoggingInstrumentor().instrument()
 logFormatter = logging.Formatter("%(pathname)s:%(funcName)s:%(lineno)d:%(levelname)s:%(message)s")
-otel_log_handler.setFormatter(logFormatter)
-logging.getLogger().addHandler(otel_log_handler)
+# otel_log_handler.setFormatter(logFormatter)
+# logging.getLogger().addHandler(otel_log_handler)
 
-# --- 4. Configure Python Logging ---
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# # --- 4. Configure Python Logging ---
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 # logger = logging.getLogger(__name__)
 
 app = FastAPI(title=app_name)
